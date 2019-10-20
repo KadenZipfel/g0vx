7bd441fe24b47290c33a7e8847e8464e242d0fc4
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-10-20 16:04:09 -0700

    Test proposal submission

---
+++ test/Governance.js
@@ -0,0 +1,15 @@
+const Governance = artifacts.require('Governance');
+
+contract('Governance', accounts => {
+  let governance;
+
+  beforeEach(async () => {
+    governance = await Governance.deployed(15);
+  });
+
+  it('Should submit proposals', async () => {
+    await governance.submitProposal("Dog");
+    const proposal = await governance.proposals(0);
+    assert(proposal.name === "Dog");
+  });
+});
