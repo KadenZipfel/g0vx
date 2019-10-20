87ed02c25b319728aa27820cb308335f6a24af9b
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-10-20 16:07:19 -0700

    Test vote for

--- test/Governance.js
+++ test/Governance.js
@@ -12,4 +12,10 @@ contract('Governance', accounts => {
     const proposal = await governance.proposals(0);
     assert(proposal.name === "Dog");
   });
+
+  it('Should allow users to vote for proposals', async () => {
+    await governance.submitVote(0, true, {from: accounts[0]});
+    const proposal = await governance.proposals(0);
+    assert(proposal.voteWeightFor > 0);
+  });
 });
