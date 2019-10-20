13f9c48ec21d763ecd838ab0c8512adb7131ce94
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-10-20 12:18:21 -0700

    Updates

---
+++ precache-manifest.9823d6ed0d680a4e55969ad76cbe97de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cf616bf34ffa27ee5a41ac99dedaefc",
+    "url": "/g0vx/static/media/github.5cf616bf.png"
+  },
+  {
+    "revision": "3406091778fd6930749f",
+    "url": "/g0vx/static/js/runtime~main.34060917.js"
+  },
+  {
+    "revision": "8d5b0a8fe25d0019ac6a",
+    "url": "/g0vx/static/js/main.8d5b0a8f.chunk.js"
+  },
+  {
+    "revision": "5a4da0b9b3a4be6476a1",
+    "url": "/g0vx/static/js/1.5a4da0b9.chunk.js"
+  },
+  {
+    "revision": "8d5b0a8fe25d0019ac6a",
+    "url": "/g0vx/static/css/main.771e463f.chunk.css"
+  },
+  {
+    "revision": "d199c033f58d9fd41efb11a2e2fbfff2",
+    "url": "/g0vx/index.html"
+  }
+];
