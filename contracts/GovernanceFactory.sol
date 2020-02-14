494e56dc0b44a1b9843e4f69ec2f0b74ad6c5d63
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-02-13 21:16:18 -0800

    Create GovernanceFactory contract

---
+++ contracts/GovernanceFactory.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.5.8;
+
+import './Governance.sol';
+
+contract GovernanceFactory {
+  mapping(uint => address) protocols;
+  uint nextId;
+
+  function createProtocol(uint _timeLimit, address _token) public {
+    require(protocols[nextId] == address(0), 'id already in use');
+    Governance protocol = new Governance(_timeLimit, _token);
+    protocols[nextId] = address(protocol);
+    nextId++;
+  }
+
+  function getProtocol(uint _id) public view returns(address) {
+    return protocols[_id];
+  }
+}
