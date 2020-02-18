583c59a6488ddb5dc2257973af0fd91f34edb547
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-02-18 15:09:33 -0800

    Update migrate script to deploy factory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Governance = artifacts.require("./Governance.sol");
+var GovernanceFactory = artifacts.require("./GovernanceFactory.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Governance, 150, '0x101848D5C5bBca18E6b4431eEdF6B95E9ADF82FA');
+  deployer.deploy(GovernanceFactory);
 };
